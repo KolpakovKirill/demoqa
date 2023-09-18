f9e7a0c15d38f50e499df2f4f92ab6b63729362c
Kirill <kolpakov7@yandex.ru>
2023-09-18 16:18:35 +0300

    add method test_check_footer_text (в test_hw\test_check_text) commit

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -2,21 +2,12 @@ from tests.tests_hw.footer_page import Footer
 from conftest import browser
 from pages.demoqa import DemoQa
 from pages.elements_page import ElementsPage
-#def test_check_footer_text(browser):
-
-    #footer_page = Footer(browser)
-    #footer_page.visit()
-
-    #assert footer_page.text_footer.exist()
-    #assert footer_page.text_footer.get_text() == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
-    #assert footer_page.text_footer.get_footer_text == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
 
 def test_check_footer_text(browser):
     footer_page = DemoQa(browser)
 
     footer_page.visit()
     assert footer_page.footer_text.get_text() == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
-
 
 
 def test_check_text_center(browser):
@@ -29,3 +20,14 @@ def test_check_text_center(browser):
     assert elements_page.text_center.get_text() == "Please select an item from left to start practice."
 
 
+
+
+
+#def test_check_footer_text(browser):
+
+    #footer_page = Footer(browser)
+    #footer_page.visit()
+
+    #assert footer_page.text_footer.exist()
+    #assert footer_page.text_footer.get_text() == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
+    #assert footer_page.text_footer.get_footer_text == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
