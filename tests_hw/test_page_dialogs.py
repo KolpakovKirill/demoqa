efe041eeb4d7dd99de34986d9737709cd33466ac
Kirill <kolpakov7@yandex.ru>
2023-09-22 15:05:48 +0300

    ДЗ 8 commit

--- tests_hw/test_page_dialogs.py
+++ tests_hw/test_page_dialogs.py
@@ -5,4 +5,4 @@ from pages.modal_dialogs import ModalDialogs
 def test_modal_elements(browser):
     modal_elements = ModalDialogs(browser)
     modal_elements.visit()
-    assert modal_elements.btn_sidebar_modal_dialogs.check_count_elements(5)
+    assert modal_elements.btn_sidebar_modal_dialogs.check_count_elements(5, int)
